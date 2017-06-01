d94d05dfa3b6e3d5feef09a6f462ff6e5b41cbe5
Luke Freeman <lukefreeman@Lukes-MacBook-Pro.local>
2017-05-31 21:10:00 -0500

    added first handlebars functionality

---
+++ handle.js
@@ -0,0 +1,11 @@
+window.onload = function doHandle(){
+  var myInfo = "<p>My name is {{name}} and I live at {{street}} in {{city}}, {{state}}</p>";
+
+  var template = Handlebars.compile(myInfo);
+
+  var data = template({name: 'Luke', street: '429 Chateau Grimaldi', city: 'Mandeville', state: 'LA'});
+
+  document.getElementById("luke").innerHTML += data;
+
+  console.log('hi');
+}
